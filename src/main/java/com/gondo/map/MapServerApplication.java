92ef3e79ee83f538268e1f52691fd4e06f743f3c
choido <choido@eseict.com>
2025-09-29 22:35:47 +0900

    feat(feat): feat
    서버 시간 설정

--- src/main/java/com/gondo/map/MapServerApplication.java
+++ src/main/java/com/gondo/map/MapServerApplication.java
@@ -1,12 +1,21 @@
 package com.gondo.map;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class MapServerApplication {
 
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
     public static void main(String[] args) {
+        System.setProperty("user.timezone", "Asia/Seoul");
         SpringApplication.run(MapServerApplication.class, args);
     }
 
